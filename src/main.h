a1f186cb45a298ea4d28bbe03e7c9f766fedb71a
VinceLW <vincelw@gmail.com>
2016-04-26 13:49:58 +0200

    Update main.h

--- src/main.h
+++ src/main.h
@@ -1,21 +1,31 @@
 #include "pebble.h"
-  
+
+#ifdef PBL_ROUND
 static const GPathInfo MINUTE_HAND_POINTS = {
   4,
-  (GPoint []) {
-    { -3, 16 },
-    { 5, 16 },
-    { 4, -66 },
-    { -2, -66 }
+ (GPoint []) {
+   { -3, 16 },
+   { 5, 16 },
+   { 4, -84 },
+   { -2, -84 }
   }
-};
-
-static const GPathInfo HOUR_HAND_POINTS = {
-  4,
-  (GPoint []) {
-    { -3, 16 },
-    { 5, 16 },
-    { 4, -45 },
-    { -2, -45 }
-  }
-};
+ };
+ 
+ static const GPathInfo HOUR_HAND_POINTS = {
+   4,
+   (GPoint []) {
+     { -3, 16 },
+     { 5, 16 },
+     { 4, -55 },
+     { -2, -55 }
+   }
+ };
+ #else
+  static const GPathInfo MINUTE_HAND_POINTS = {
+    4,
+    (GPoint []) {
+ @@ -19,3 +40,4 @@ static const GPathInfo HOUR_HAND_POINTS = {
+      { -2, -45 }
+    }
+  };
+ #endif
